5e1af0bbe2a9f2d3b41ec53cbf93d23626760007
Isaak Morales <ice.isaak97@gmail.com>
2022-03-23 21:08:24 -0400

    Added all the rows with new ID's

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,20 @@
-//TODO GIVEN I am using a daily planner to create a schedule
-//TODO WHEN I open the planner
-//TODO THEN the current day is displayed at the top of the calendar
 //*Setting time in header
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
-//TODO WHEN I scroll down
-//TODO THEN I am presented with timeblocks for standard business hours
 
-//TODO WHEN I view the timeblocks for that day
-//TODO THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 
-//TODO WHEN I click into a timeblock
-//TODO THEN I can enter an event
+//! WHEN I view the timeblocks for that day
+//!  THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 
-//TODO WHEN I click the save button for that timeblock
-//TODO THEN the text for that event is saved in local storage
+
+
+//! WHEN I click the save button for that timeblock
+//! THEN the text for that event is saved in local storage
 //* Local Storage
 
 $(".saveBtn").on("click", function () {
-    var userText = 
+    localStorage.setItem("Key", JSON.stringify)
 })
 
-//TODO WHEN I refresh the page
-//TODO THEN the saved events persist
+//! WHEN I refresh the page
+//! THEN the saved events persist
